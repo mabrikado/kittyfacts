7c4f4089aeeb2cb6c93cbc56c0ff6d1087cf6c89
Sibusiso Buthelezi <sibuthejhb124@student.wethinkcode.co.za>
2025-06-30 16:58:08 +0200

    Finished template setup

--- kittyfacts/main/views.py
+++ kittyfacts/main/views.py
@@ -4,10 +4,10 @@ from .models import CatFact
 # Create your views here.
 def home(request):
     randomFact = CatFact.objects.order_by('?').first()
-    return HttpResponse(randomFact)
+    return render(request, "home.html", {"title":"KittyFacts" , "fact_title":randomFact.title , "fact_description":randomFact.description})
 
 def about(request):
-    return HttpResponse('About KittyFacts')
+    return render(request, "about.html", {"title":"About KittyFacts"})
 
 def myCatStory(request):
-    return HttpResponse('Story of my cat')
+    return render(request, "story.html", {"title":"Story of Kitty"})
